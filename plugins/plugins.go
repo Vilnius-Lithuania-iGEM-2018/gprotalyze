ce47ccb98d4240ec4eaccbe36192eb8dc255619f
devblok <l.praninskas@gmail.com>
2018-05-27 18:44:18 +0300

    Fix lint errors

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -2,12 +2,15 @@
 // for data supply and storage options and possibly other plugins
 package plugins
 
+// PluginContext describes an analysis plugin
 type PluginContext struct {
 	Name     string
 	FilePath string
 	Version  string
 }
 
+// Plugin contains language specific details
+// and whatnot, handles all aspects of it.
 type Plugin interface {
 	Run() error
 	GetContext() *PluginContext
